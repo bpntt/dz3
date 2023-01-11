20db0bbb5c36ea015e50639cf4dde08dacafe424
BPNTT <haataya@163.com>
2023-01-11 16:53:13 +0600

    2ways

---
+++ 002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координату X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+int quarter = Quarter(x, y);
+int Quarter (int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;
+}
+string result = quarter > 0 
+? $"{quarter}"
+: "Координата не может быть равной 0"; 
+Console.WriteLine(result);
