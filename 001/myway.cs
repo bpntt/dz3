0468404538cbab36a701d0426ca587fe964542a7
BPNTT <haataya@163.com>
2023-01-12 05:47:35 +0600

    1-3

---
+++ 001/myway.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите координату X");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Недопустимые значения X,Y");
+}
+if (x  > 0 || y > 0 )
+{
+    Console.WriteLine("1/4");
+    return;
+}
+if (x  > 0 || y < 0 )
+{
+    Console.WriteLine("4/4");
+    return;
+}
+if (x < 0 || y < 0 )
+{
+    Console.WriteLine("3/4");
+    return;
+}
+if (x < 0 || y > 0 )
+{
+    Console.WriteLine("2/4");
+    return;
+}
---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ввведите номер четверти");
+int a = Convert.ToInt32(Console.ReadLine());
+string Di = D (a);
+Console.WriteLine($"{Di}");
+string  D (int Z)
+{
+    if (a == 1) return "x > 0, y > 0";
+    if (a == 2) return "x > 0, y < 0";
+    if (a == 3) return "x < 0, y < 0";
+    if (a == 4) return "x < 0, y > 0";
+    return "not correct";
+}
